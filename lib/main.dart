6cc858d586fe1d0c2a945f4c842b12867f5beec1
Omar Magdy <omarmagdyshawky@gmail.comm>
2020-05-23 16:06:01 +0200

    draw all bottons

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Calculator",
       home: Calculator(),
     );
@@ -50,20 +51,47 @@ class _CalculatorState extends State<Calculator> {
               ],
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(bottom: 10.0),
-                  child: RaisedButton(
-                    onPressed: () {},
-                    child: Text(
-                      "C",
-                      style: TextStyle(fontSize: 30),
-                    ),
-                    padding: EdgeInsets.all(20),
-                    color: Colors.amber,
-                    shape: CircleBorder(),
-                  ),
-                ),
+                drawbtn("C", Colors.amber),
+                drawbtn("+/-", Colors.white),
+                drawbtn("%", Colors.amber),
+                drawbtn("/", Colors.amber),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                drawbtn("7", Colors.grey),
+                drawbtn("8", Colors.grey),
+                drawbtn("9", Colors.grey),
+                drawbtn("X", Colors.amber),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                drawbtn("4", Colors.grey),
+                drawbtn("5", Colors.grey),
+                drawbtn("6", Colors.grey),
+                drawbtn("-", Colors.amber),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                drawbtn("1", Colors.grey),
+                drawbtn("2", Colors.grey),
+                drawbtn("3", Colors.grey),
+                drawbtn("+", Colors.amber),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                drawbtn("0", Colors.grey),
+                drawbtn(".", Colors.grey),
+                drawbtn("=", Colors.amber),
               ],
             ),
           ],
@@ -71,4 +99,20 @@ class _CalculatorState extends State<Calculator> {
       ),
     );
   }
+}
+
+Widget drawbtn(String btnText, Color color) {
+  return Container(
+    margin: EdgeInsets.only(bottom: 10.0),
+    child: RaisedButton(
+      onPressed: () {},
+      child: Text(
+        btnText,
+        style: TextStyle(fontSize: 30),
+      ),
+      padding: EdgeInsets.all(20),
+      color: color,
+      shape: CircleBorder(),
+    ),
+  );
 }
